cf4be181952958e92e654b8702cde3273d0190d6
hzlabunishtaitalia <info.labunishta.italia@gmail.com>
2026-06-08 09:37:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function toggleMenu(){document.querySelector('.links').classList.toggle('open')}
+function copyIban(){navigator.clipboard.writeText('IT65Z0200861624000107426851');alert('IBAN copiato')}
